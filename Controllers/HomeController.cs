84402d27d3c54a65ef216979ab64655f6e382de5
Eduardo Mardini <emardini@pdisoftware.com>
2019-11-30 17:54:49 -0500

    Add map and markers

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,11 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
+﻿
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using filteredMap.Models;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.Linq;
 
 namespace filteredMap.Controllers
 {
@@ -18,9 +20,16 @@ namespace filteredMap.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var httpClient = new HttpClient();
+            var result = await httpClient.GetStringAsync("https://maps.googleapis.com/maps/api/place/textsearch/json?query=parks+in+Virginia+Beach+,+VA&key=AIzaSyAtbBkcrGo97HSiQRD3seMq3NxwD6K5RWA");
+
+            var placeSearchResult = JsonConvert.DeserializeObject<PlaceSearchResult>(result) ?? new PlaceSearchResult { Results = new List<GPlace> { } };
+
+            var places = placeSearchResult.Results.Select(x => new Place { Address = x.Formatted_Address, Location = new Location { Latitude = x.Geometry.Location.Lat, Longitude = x.Geometry.Location.Lng }, Name = x.Name });
+
+            return View(places);
         }
 
         public IActionResult Privacy()
@@ -34,4 +43,45 @@ namespace filteredMap.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
+}
+
+
+public class GLocation
+{
+    public double Lat { get; set; }
+    public double Lng { get; set; }
+}
+
+public class Geometry
+{
+    public GLocation Location { get; set; }
+}
+
+public class GPlace
+{
+    public Geometry Geometry { get; set; }
+    public string Formatted_Address { get; set; }
+
+    public string Name { get; set; }
+}
+
+public class PlaceSearchResult
+{
+    public IEnumerable<GPlace> Results { get; set; }
+}
+
+
+public class Location
+{
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+}
+
+public class Place
+{
+    public string Address { get; set; }
+
+    public string Name { get; set; }
+
+    public Location Location { get; set; }
 }
